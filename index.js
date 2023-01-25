e3670a92f79b662a9827f43a9a159fdea6c3c1eb
John Doe <johndoe@example.com>
2023-01-25 12:01:29 -0500

    collectibles

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ skullyImage.src = "../images/player.png"
 const collectible = new Image()
 collectible.src = "../images/collectible.png"
 
-const startingX = canvas.width/2 - 25
-const startingY = canvas.height - 120
+let startingX = 540
+let startingY = 400
 
 
 
-let collectiblesIntervalId
+let createCollectiblesId
 let animationLoopId
-let gravityLoopId
+// let gravityLoopId
 
 let gameOn = false
 
@@ -25,185 +25,215 @@ let score = 0
 
 
 
-const skully = {
-  x: 400, 
-  y: 200, 
-  width: 50, 
-  height: 100, 
-  speedX: 0, 
-  speedY: 0, 
-  gravity: 0.1, 
-  gravitySpeed: 0, 
-  update: function() {
-    console.log("Gravity speed", this.gravitySpeed)
-    this.gravitySpeed = this.gravitySpeed + this.gravity
-  }, 
-  newPosition: function() {
-    this.y = this.y + this.gravitySpeed
-  }
-  
-}
-
-
 class Collectible {
-  constructor() {
-    this.x = Math.random() * 700; 
-    
-    this.y = 0; 
+constructor() {
+  this.x = Math.random() * 700; 
+  this.y = 0; 
+  this.width = 20 + Math.floor(Math.random() * 350);    
+  this.height = 20; 
 
-    this.width = 20 + Math.floor(math.random() * 350); 
-    
-    this.height = 20; 
+  
+}
 
-    
-  }
-  
-  update() {
-    this.sharedX = this.sharedX - 2
-  }
-  
-  draw() {
-    ctx.drawImage(collectible, this.x, this.y, this.width, this.height)
-  }
+newPosition() {
+  this.y++ 
 }
 
 
-function checkCollision(collectible) {
-  if (skully.y < collectible.y + collectible.height
-    && collectible.y < skully.y + skully.height 
-    &&  collectible.x < skully.x + skully.width 
-    && collectible.x + collectible.width > skully.x) {
-      gameOver() 
+draw() {
+  ctx.drawImage(collectible, this.x, 100, 200, 150) 
+}
+}
+
+
+const skully = {
+  
+    x: startingX, 
+    y: startingY, 
+    width: 120, 
+    height: 100, 
+  
+    draw: function() {
+   
+     
+      ctx.drawImage(skullyImage, this.x, this.y, this.width, this.height)
+
+    }, 
+  
+    moveLeft: function() {
+      this.x = this.x - 15
+    }, 
+  
+    moveRight: function() {
+      this.x = this.x + 15
+    },
+  
+    moveUp: function() {
+      this.y = this.y - 15
+    }, 
+  
+    moveDown: function() {
+      this.y = this.y + 15 
     }
-}
+  }
 
-
-let collectiblesArray = []
-
-function createCollectibles () {
-
-  collectiblesIntervalId = setInterval(() => {
-    collectiblesArray.push(new Collectible())
-    console.log("Collectibles:", collectiblesArray)
-  }, 3750)
-}
-
-function animationLoop () {
-  animationLoopId = setInterval(() => {
-    
-    skully.newPosition()
-    
-    ctx.clearRect(0, 0, 1200, 600)
-    
-    ctx.drawImage(background, 0, 0, 1200, 600)
-    
-    ctx.drawImage(skullyImage, skully.x, skully.y, 75, 50)
-    
-    for (let i = 0; i < collectiblesArray.length; i++) {
-      if (collectiblesArray[i].sharedX < -138) {
-        collectiblesArray.splice(i, 1)
+  function checkCollision(collectible) {
+    if (skully.y < collectible.y + collectible.height
+      && collectible.y < skully.y + skully.height 
+      &&  collectible.x < skully.x + skully.width 
+      && collectible.x + collectible.width > skully.x) {
+        gameOver() 
       }
-      collectiblesArray[i].update()
+  }
+
+  let collectiblesArray = []
+  
+  function createCollectibles() {
+  
+    createCollectiblesId = setInterval(() => {
+      collectiblesArray.push(new Collectible())
+    }, 3750)
+  }
+  
+  function animationLoop() {
+    animationLoopId = setInterval(() => {
+      
+      
+      ctx.clearRect(0, 0, 1200, 600)
+      
+      ctx.drawImage(background, 0, 0, 1200, 600)
+      
+      
+      skully.draw()
+    // ctx.drawImage(skullyImage, 100, 100, 120, 100)
+      for (let i = 0; i < collectiblesArray.length; i++) {
+        if (collectiblesArray[i].sharedX < -138) {
+          collectiblesArray.splice(i, 1)
+        }
+        // collectiblesArray[i].update()
+        collectiblesArray[i].draw()
+      }
+    }, 16)
+  }
+
+
+  function showScore() {
+
+    ctx.fillStyle = 'black'
+    ctx.fillRect(340, 10, 150, 50)
+  
+    ctx.fillStyle = 'white'
+    ctx.font = '24px serif'
+    ctx.fillText(`Score: ${score}`, 370, 40)
+  }
+
+  function updateCanvas() {
+  
+    ctx.clearRect(0, 0, 500, 700)
+  
+    ctx.drawImage(background, 0, 0, 500, 700)
+  
+    
+    skully.draw() 
+    for (let i = 0; i < collectiblesArray.length; i++) {
+      if (collectiblesArray[i].y > canvas.height) {
+        collectiblesArray.splice(i, 1) 
+        score++ 
+        console.log("This is the score", score, collectiblesArray)
+      }
+      checkCollision(collectiblesArray[i])
+      collectiblesArray[i].newPosition()
       collectiblesArray[i].draw()
     }
-  }, 16)
-}
-
-function showScore() {
-  ctx.fillStyle = 'black'
-  ctx.fillRect(340, 10, 150, 50)
-
-  ctx.fillStyle = 'white'
-  ctx.font = '24px serif'
-  ctx.fillText(`Score: ${score}`, 370, 40)
-}
-
-function updateCanvas() {
-
-  ctx.clearRect(0, 0, 500, 700)
-
-  ctx.drawImage(background, 0, 0, 500, 700)
-
-  
-  skully.draw() 
-  for (let i = 0; i < collectiblesArray.length; i++) {
-    if (collectiblesArray[i].y > canvas.height) {
-      collectiblesArray.splice(i, 1) 
-      score++ 
-      console.log("This is the score", score, collectiblesArray)
-    }
-    checkCollision(collectiblesArray[i])
-    collectiblesArray[i].newPosition()
-    collectiblesArray[i].draw()
-  }
-  
-  showScore()
-  if (score === 15) {
-    gameOver
-  }
-
-}
-
-
-function gravityLoop () {
-  gravityLoopId = setInterval(() => {
-    skully.update ()
-  }, 50)
-  
-  
-}
-
-function startGame() {
-   console.log("Starting the game")
-   console.log("Background:", background)
-
-   gaemOn = true
-   
-   collectiblesArray = []
-   skully.x = startingX; 
-   skully.y = startingY; 
-
-
-
-   canvas.width = "1200"
-   canvas.height = "600"
-   canvas.style.visibility = "visible"
-   ctx.drawImage(background, 0, 0, 1200, 600)
-
-  animationLoop()
-  createCollectibles()
-  gravityLoop()
     
+    showScore()
+    if (score === 15) {
+      gameOver
+    }
     
   }
-
-  function gameOver () {
-
-    gameOn = false 
-
-    clearInterval(animationLoopId)
-    clearInterval(gravityLoopId)
-    clearInterval(createCollectibles)
-
-
-    ctx.clearRect(0, 0, 500, 700)
-    ctx.fillStyle = 'black'
-    ctx.fillRect(0, 0, 500, 700)
-
-    if (scre > 14) {
-      ctx.fillStyle = 'white'
-      ctx.font = '40px serif'
-      ctx.fillText("You've won!", 150, 200)
-    } else {
-      ctx.fillStyle = 'white'
-      ctx.font = '40px serif'
-      ctx.fillText("You've lost!", 150, 200)
+  
+  function startGame() {
+     console.log("Starting the game")
+    
+  
+     gaemOn = true
+     
+     collectiblesArray = []
+     skully.x = startingX; 
+     skully.y = startingY; 
+  
+  
+  
+     canvas.width = "1200"
+     canvas.height = "600"
+     canvas.style.visibility = "visible"
+    //  ctx.drawImage(background, 0, 0, 1200, 600)
+  
+    animationLoop()
+    createCollectibles()
+    // gravityLoop()
+      
+      
     }
+    
+    function gameOver () {
+    
+      gameOn = false 
+    
+      clearInterval(animationLoopId)
+      // clearInterval(gravityLoopId)
+      clearInterval(createCollectiblesId)
+    
+    
+      ctx.clearRect(0, 0, 500, 700)
+      ctx.fillStyle = 'black'
+      ctx.fillRect(0, 0, 500, 700)
+    
+      if (score > 14) {
+        ctx.fillStyle = 'white'
+        ctx.font = '40px serif'
+        ctx.fillText("You've won!", 150, 200)
+      } else {
+        ctx.fillStyle = 'white'
+        ctx.font = '40px serif'
+        ctx.fillText("You've lost!", 150, 200)
+      }
+    
+      collectiblesArray = []
+      score = 0
+    
+    }
+  
+  
 
-    collectiblesArray = []
-    score = 0
 
-  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function gravityLoop () {
+//   gravityLoopId = setInterval(() => {
+//     skully.update ()
+//   }, 50)
+  
+  
+// }
+
+
 
 
 window.onload = function() {
@@ -214,12 +244,25 @@ window.onload = function() {
       }
     };
 
+    
+
     document.addEventListener('keydown', e => {
       switch (e.keyCode) {
-        case 38: 
-        skully.gravitySpeed = skully.gravitySpeed - 0.2;
-        break; 
+        case 38:
+          skully.moveUp();
+          break;
+        case 40:
+          skully.moveDown();
+          break;
+        case 37:
+          skully.moveLeft();
+          break;
+        case 39:
+          skully.moveRight();
+  
+          break;
       }
-    })
+      // updateCanvas();
+    });
     
     };
